babea9946e633ba27f4f9b31d63b8352954aa042
re0078 <reza.abdollahzadeh@gmail.com>
2019-11-07 14:25:53 +0330

    not complete phase 2

--- web/views.py
+++ web/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'homepage.html',)
+    return render(request, 'homepage.html', )
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            last_name = form.cleaned_data.get('last_name')
+            first_name = form.cleaned_data.get('first_name')
+            username = form.cleaned_data.get('username')
+            password1 = form.cleaned_data.get('password1')
+            password2 = form.cleaned_data.get('password2')
+            email = form.cleaned_data.get('email')
+            user = authenticate(first_name=first_name,
+                                last_name=last_name,
+                                username=username,
+                                password1=password1,
+                                password2=password2,
+                                email=email)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
