92e03f393a859f68bdf09fde2cd6126799baf410
re0078 <reza.abdollahzadeh@gmail.com>
2019-11-07 16:27:08 +0330

    phase #2.3

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from web.views import index, register, login
+from web.views import index, register, login_user
 
 app_name = "web"
 urlpatterns = [
     path('', index, name='homepage'),
     path('register', register, name='register'),
-    path('login', login, name='login'),
+    path('login', login_user, name='login'),
 ]
--- web/views.py
+++ web/views.py
@@ -7,7 +7,7 @@ from web.forms import RegisterForm
 
 
 def index(request):
-    return render(request, 'homepage.html', )
+    return render(request, 'homepage.html', {'logged_in': request.user.is_authenticated})
 
 
 def register(request):
@@ -15,11 +15,23 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('web:homepage')
+            return redirect('web:register')
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
 
 
-def login(request):
-    return render(request, 'login.html',)
+def login_user(request):
+    error = None
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            error = 'user not found'
+            return render(request, 'login.html', {'error': error})
+        else:
+            login(request, user)
+            redirect('web:homepage')
+    else:
+        return render(request, 'login.html', {'error': error})
